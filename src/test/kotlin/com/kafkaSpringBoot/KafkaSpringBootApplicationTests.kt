33b565dd76326a9f8777a866e5e95bd46bb24f8d
Gustavo Rolan Soares <78105046+gustavorolan@users.noreply.github.com>
2023-01-26 16:02:14 -0300

    init

---
+++ src/test/kotlin/com/kafkaSpringBoot/KafkaSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kafkaSpringBoot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
